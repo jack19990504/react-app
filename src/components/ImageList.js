3b2910e492b5a244867d7257d90c4a0f66946077
jack19990504 <jack19990504@gmail.com>
2020-12-26 21:42:34 +0800

    finish ch10

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,13 +1,14 @@
-import { image } from "faker";
-import React from "react";
+import React from 'react';
+import './ImageList.css';
+import ImageCard from './imageCard'
 
 const ImageList = props => {
 
-    const images = props.images.map(( { description, id, urls} ) => {
-        return <img alt = { description } key = { id } src = { urls.regular }/>
+    const images = props.images.map((image) => {
+        return <ImageCard key = {image.id} image = {image}/> 
     });
 
-    return <div>{images}</div>
+    return <div className = "image-list"> { images } </div>
 };
 
 export default ImageList;
